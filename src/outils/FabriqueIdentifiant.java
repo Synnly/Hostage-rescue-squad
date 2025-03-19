23ee26f8d458733099390fc6d5f72568ed68f734
morad-aiachi-dashfile-at-394109254195 <aiachi3u@etu.univ-lorraine.fr>
2025-03-19 16:08:06 +0100

    Tests Fabrique identifiant

--- src/outils/FabriqueIdentifiant.java
+++ src/outils/FabriqueIdentifiant.java
@@ -3,10 +3,13 @@ package outils;
 public class FabriqueIdentifiant {
     private static FabriqueIdentifiant instance =  new FabriqueIdentifiant();
     private int identifiant = 0;
-    public FabriqueIdentifiant getInstance(){
+    public static FabriqueIdentifiant getInstance(){
         return instance;
     }
     public int getId(){
         return identifiant++;
+    }
+    public void resetId(){
+        identifiant = 0;
     }
 }
--- test/testOutils/TestFabriqueIdentifiant.java
+++ test/testOutils/TestFabriqueIdentifiant.java
@@ -1,4 +1,47 @@
 package testOutils;
 
+
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import outils.FabriqueIdentifiant;
+import static org.junit.Assert.*;
+
+
 public class TestFabriqueIdentifiant {
+
+
+
+/*Reset est appelé en début de fonction pour ne pas générer d'erreurs lors de
+l'exécution séquentielle des tests
+
+ */
+    @Test
+    @DisplayName("Test de sur les valeurs de fabrique identifiant")
+    public void testDeLincrementationDesIdentifiants() {
+        FabriqueIdentifiant.getInstance().resetId();
+        assertEquals(FabriqueIdentifiant.getInstance().getId(), 0);
+        assertEquals(FabriqueIdentifiant.getInstance().getId(), 1);
+        assertEquals(FabriqueIdentifiant.getInstance().getId(), 2);
+        assertEquals(FabriqueIdentifiant.getInstance().getId(), 3);
+        assertEquals(FabriqueIdentifiant.getInstance().getId(), 4);
+
+    }
+
+    @Test
+    @DisplayName("Test de la remise à zéro des identifiant")
+    public void testDuResetDesIdentifiants() {
+        FabriqueIdentifiant.getInstance().resetId();
+        assertEquals(FabriqueIdentifiant.getInstance().getId(), 0);
+        assertEquals(FabriqueIdentifiant.getInstance().getId(), 1);
+        assertEquals(FabriqueIdentifiant.getInstance().getId(), 2);
+        assertEquals(FabriqueIdentifiant.getInstance().getId(), 3);
+        assertEquals(FabriqueIdentifiant.getInstance().getId(), 4);
+        FabriqueIdentifiant.getInstance().resetId();
+        assertEquals(FabriqueIdentifiant.getInstance().getId(), 0);
+
+
+
+    }
+
 }
+
