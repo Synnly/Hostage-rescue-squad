4116a0fbc55b5b7c6a9bc9264434ebfce0da300e
Emanuel Fernandes dos Santos <emanuel.fernandes-dos-santos4@etu.univ-lorraine.fr>
2025-04-17 09:50:34 +0200

    Ajout fichiers manquants

---
+++ src/vues/VueAvertissement.java
@@ -0,0 +1,50 @@
+package vues;
+
+import coups.Coup;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import mdp.Direction;
+import observable.Environnement;
+import observable.Observable;
+import org.javatuples.Pair;
+import vues.Observer;
+
+import java.util.Map;
+
+public class VueAvertissement extends Observer {
+    private final Environnement env;
+
+    @FXML
+    private TextArea textAvertissement;
+
+    /**
+     * Constructeur d'un observateur type
+     *
+     * @param sujet Le sujet
+     */
+    public VueAvertissement(Environnement sujet) {
+        super(sujet);
+        this.env = sujet;
+        sujet.ajouterObserver(this);
+    }
+
+    @Override
+    public void update() {
+        Map<Pair<Coup, Direction>, Double> danger = env.predireDanger();
+        textAvertissement.setText("");
+
+        StringBuilder sb = new StringBuilder("");
+        for(Pair<Coup, Direction> coup : danger.keySet()){
+            sb.append(coup.getValue0()).append(" vers ").append(coup.getValue1()).append(" : ").append(100 * danger.get(coup)).append(" %\n");
+        }
+
+        textAvertissement.setText(sb.toString());
+    }
+
+    @Override
+    public void handle(ActionEvent actionEvent) {
+
+    }
+}
